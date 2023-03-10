63eedffbd3021805e6da01ecf9eb874df4ac0af9
IAPA2309 <iniaki.perezaviles@gmail.com>
2023-03-10 00:11:09 -0300

    arreglar color si valido

--- main.js
+++ main.js
@@ -1,33 +1,99 @@
-var form = document.getElementById("Form");
-let notaMatematica = document.getElementById('inputMatematica');
-let notaLengua = document.getElementById('inputLengua');
-let notaEfsi = document.getElementById('inputEfsi');
+const form = document.getElementById("Form");
+let inputMath = document.getElementById('inputMatematica');
+let inputLanguage = document.getElementById('inputLengua');
+let inputEfsi = document.getElementById('inputEfsi');
 let resultado = document.getElementById('Resultado');
-let btnPromedio = document.getElementById('Promedio');
 
 
+// Para los 3 inputs, cambio el color segun el valor
+inputMath.onkeyup = (e) => {
+    e.target.style.color = "red";
 
-notaLengua.onkeydown = (e) => {
-    console.log(e.target.value);
-    resultado.innerHTML = e.target.value;
+    isValidScore(e.target.value, () => {
+      e.target.style.color = "green";
+    });
 }
 
-notaMatematica.onkeydown = (e) => {
-    console.log(e.target.value);
+inputLanguage.onkeyup = (e) => {
+    e.target.style.color = "red";
+    
+    isValidScore(e.target.value, () => {
+        e.target.style.color = "green"
+    });
 }
 
-notaEfsi.onkeydown = function(e){
-    console.log(e.target.value);
+inputEfsi.onkeyup = (e) => {
+    e.target.style.color = "red";
+
+    isValidScore(e.target.value, () => {
+      e.target.style.color = "green";
+    });
 }
 
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let nums = Array.from(form.querySelectorAll('input[type="number"]'));
+    nums = nums.map(numero => numero.value);
+
+    if(e.submitter == document.getElementById("Promedio")){
+
+        if(isValidScore(nums) == false){
+            alert("Error. El valor debera ser entre 1 y 10 inclusives.");
+        }
+
+        resultado.innerHTML = calculateAverage(nums);
+    };
+
+    if(e.submitter == document.getElementById("MayorNota")){ 
+
+        resultado.innerHTML = searchHighestScore(nums);
+
+    };
+})
 
 
-function isValidScore(num, callback){
-    if(num > 0 && num <= 10){
-        callback();
+// Compruebo si el numero es valido, y ejecuto una funcion
+function isValidScore(nums, num, callback){
+    
+    if (num > 0 && num <= 10) {
+      callback();
     }
-
+    
+    if(nums != undefined){
+        for (let i = 0; i < nums.length; i++) {
+          if (!(+nums[i] > 0) || !(+nums[i] <= 10)) {
+            console.log(false);
+            return false;
+          }
+        }
+        console.log(true);
+        return true;
+    }
 }
 
+function calculateAverage(nums){
+    let suma = 0;
+    let promedio = 0;
+    // Recorro el array y sumo sus numeros
+    nums.forEach(num => {
+        suma += +num; // (+num) String => Number
+    });
+    promedio = (suma / nums.length); // Saco el promedio
+    promedio = Number.isInteger(promedio) ? promedio : promedio.toFixed(2); // En caso de que no sea un numero entero mustro los dos digitos despues del punto decimal
+    return promedio;
+}
 
+function searchHighestScore(nums){
+    let highest = 0;
 
+    nums.forEach(num => {
+        if(highest > +num){
+            return;
+        }
+        highest = +num;
+    });
+
+    return highest;
+}
